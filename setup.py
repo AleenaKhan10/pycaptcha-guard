d31a496d4c0fc6f585c33da95ffabbab975b78f7
AleenaKhan10 <aleenakhanraees40@gmail.com>
2024-01-24 11:17:47 +0500

    added the new version 0.1.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pycaptcha-guard",
-    version="0.1.8",
+    version="0.1.9",
     author="AleenaMuskan",
     author_email="aleenakhanraees40@gmail.com",
     description="Solve any kind of captcha like human",
